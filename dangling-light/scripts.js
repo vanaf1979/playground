fe66d47ee437abf4c37fd49e87d25516feeeaaf8
Stephan Nijman <stephan@MinivanStephan.home>
2021-03-02 14:17:13 +0100

    added dangling lights

---
+++ dangling-light/scripts.js
@@ -0,0 +1,31 @@
+const lamp = document.querySelector('.lamp');
+
+gsap.fromTo(lamp, {
+    rotate: '-12deg'
+}, {
+    rotate: '12deg',
+    duration: 1,
+    repeat:-1,
+    yoyo: true,
+    ease: Power1.easeInOut
+});
+
+
+const mask = document.querySelector('.mask');
+
+const config = {
+    deg: 1.57
+}
+
+gsap.fromTo(config, {
+    deg: 1.35
+}, {
+    deg: 1.75,
+    onUpdate: function() {
+        mask.style.setProperty('--deg', config.deg);
+    },
+    duration: 1,
+    repeat:-1,
+    yoyo: true,
+    ease: Power1.easeInOut
+});
