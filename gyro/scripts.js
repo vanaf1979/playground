94b8b86fb317a37974b7b06a83055c8fd73545d1
Vanaf1979 <vanaf1979@gmail.com>
2022-04-14 16:05:21 +0200

    Added gyro

--- gyro/scripts.js
+++ gyro/scripts.js
@@ -1,23 +1,25 @@
-const init = () => {
-    if (window.DeviceOrientationEvent) {
-        document.addEventListener('deviceorientation', deviceOrientationHandler, false);
-        const absolute = document.getElementById('absolute');
-        absolute.value = JSON.stringify('Supported');
-    } else {
-        absolute.value = JSON.stringify('Nope');
-    }
-}
-
 const deviceOrientationHandler = (e) => {
     const absolute = document.getElementById('absolute');
     const alpha = document.getElementById('alpha');
     const beta = document.getElementById('beta');
     const gamma = document.getElementById('gamma');
+    const all = document.getElementById('all');
 
     absolute.value = JSON.stringify(e.absolute);
     alpha.value = JSON.stringify(e.alpha);
     beta.value = JSON.stringify(e.beta);
     gamma.value = JSON.stringify(e.gamma);
+    all.value = JSON.stringify(e);
+}
+
+const init = () => {
+    if (window.DeviceOrientationEvent) {
+        document.addEventListener('deviceorientation', deviceOrientationHandler, false);
+        const absolute = document.getElementById('absolute');
+        absolute.value = JSON.stringify('Supportedd');
+    } else {
+        absolute.value = JSON.stringify('Nope');
+    }
 }
 
 init();
